50a1c27e069236dac08a9b1be280f974cebff9ee
hp0404 <hryhorii.pavlenko@gmail.com>
2022-08-16 18:13:44 +0300

    move defaults into separate file

--- worldcat/worldcat.py
+++ worldcat/worldcat.py
@@ -1,44 +1,11 @@
+import collections
 import re
 import typing
-import collections
 from pathlib import Path
 
+from worldcat.config import DEFAULT_FIELDNAMES
+
 JSON = typing.Dict[str, typing.Any]
-DEFAULT_FIELDNAMES = [
-    "Abstract",
-    "Access",
-    "Availability",
-    "External Resources",
-    "Find Items About",
-    "Title",
-    "Publication",
-    "Author(s)",
-    "Corp Author(s)",
-    "Publication",
-    "Year",
-    "Description",
-    "Dissertation",
-    "Language",
-    "ISBN",
-    "Genre/Form",
-    "Materials specified",
-    "Identifier",
-    "Action Note",
-    "Note(s)",
-    "Descriptor",
-    "Geographic",
-    "General Info",
-    "Class Descriptors",
-    "Other Titles",
-    "Responsibility",
-    "Material Type",
-    "Document Type",
-    "Date of Entry",
-    "Update",
-    "Accession No",
-    "Provider",
-    "Database",
-]
 
 
 class Parser:
