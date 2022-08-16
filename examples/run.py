043431de6d66125c07bfad04c0c498549b7e1cf4
hp0404 <hryhorii.pavlenko@gmail.com>
2022-08-16 22:11:06 +0300

    move example into separate dir

---
+++ examples/run.py
@@ -0,0 +1,16 @@
+import worldcat
+from pathlib import Path
+
+
+def main() -> int:
+    assets = Path(__file__).resolve().parent.parent / "assets"
+    input_file = assets / "raw" / "DirectExport.txt"
+    parser = worldcat.Parser()
+    data = worldcat.load(input_file, encoding="cp1251", implementation=parser)
+    output_file = assets / "export.json"
+    worldcat.save(data, output_file)
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
