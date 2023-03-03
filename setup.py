333832b9816466de5d2b8e94dbe1c1d5a8465f52
hp0404 <hryhorii.pavlenko@gmail.com>
2023-03-04 00:48:39 +0200

    add pre-commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 extras_requirements = {
-    "dev": ["wheel", "black", "pytest", "mypy"],
+    "dev": ["wheel", "pytest"],
 }
 
 setuptools.setup(
